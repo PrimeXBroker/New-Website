d49b04060b9c90af0b29f2b7cb451354a617ffd0
primexjarko <hassan.jarko@primexbroker.com>
2024-07-16 17:11:13 +0400

    updated

---
+++ components/GradiantButton.jsx
@@ -0,0 +1,10 @@
+const GradiantButton = ({name}) => {
+    return (
+        <button className="group w-[120px] relative overflow-hidden rounded-full bg-[#3F3F3E] px-8 py-2 text-[#FFD000] transition-all duration-300 ease-out hover:bg-gradient-to-r hover:from-[#3F3F3E] hover:to-[#5B5B5B] hover:ring-2 hover:ring-[#3F3F3E] hover:ring-offset-2">
+        <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-[#e3e4e5] opacity-10 transition-all duration-700 group-hover:-translate-x-40"></span>
+        <span className="relative flex items-center justify-center">{name}</span>
+      </button>
+    )
+}
+
+export default GradiantButton;
--- components/Header.jsx
+++ components/Header.jsx
@@ -16,6 +16,7 @@ import LocaleLink from "./LocaleLink";
 import { usePathname } from "next/navigation";
 import { RiArrowDownSLine } from "react-icons/ri";
 import { useTranslations } from "next-intl";
+import GradiantButton from "./GradiantButton";
 const Header = () => {
   const t = useTranslations("home.menu");
   const pathname = usePathname();
@@ -115,30 +116,33 @@ const Header = () => {
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#" className={NavHoverEffect}>
+          
             Gift Account
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Link href="#">
+          <GradiantButton name="Register"/>
+          {/* <Link href="#">
             <button className="group rounded-full relative min-h-[40px] w-32 overflow-hidden border border-[#3F3F3E] bg-secondary text-[#FFD000] shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#FFD000] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#FFD000] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full">
               <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#FFD000] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#FFD000] after:duration-500 hover:text-secondary hover:font-semibold group-hover:before:h-full group-hover:after:h-full"></span>
               <span className="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-secondary group-hover:font-semibold">
                 {t("register")}
               </span>
             </button>
-          </Link>
+          </Link> */}
         </NavbarItem>
         <NavbarItem>
-          <Link href="#">
+        <GradiantButton name="Login"/>
+          {/* <Link href="#">
             <button className="group rounded-full relative min-h-[40px] w-32 overflow-hidden border border-[#3F3F3E] bg-secondary text-[#FFD000] shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#FFD000] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#FFD000] after:duration-500 hover:text-white hover:before:h-full hover:after:h-full">
               <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#FFD000] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#FFD000] after:duration-500 hover:text-secondary hover:font-semibold group-hover:before:h-full group-hover:after:h-full"></span>
               <span className="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-secondary group-hover:font-semibold">
                 Login
               </span>
             </button>
-          </Link>
+          </Link> */}
         </NavbarItem>
         <NavbarItem>
           <div className="dropdown">
