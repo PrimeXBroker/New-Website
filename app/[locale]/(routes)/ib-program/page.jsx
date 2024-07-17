13c3539ebc334cb686d9dc10873c930e0f8d5d5e
PrimeXBroker <it@primexbroker.com>
2024-07-17 13:22:34 +0400

    locale

--- app/[locale]/(routes)/ib-program/page.jsx
+++ app/[locale]/(routes)/ib-program/page.jsx
@@ -1,11 +1,17 @@
+// "use client"
 import Banner from "@/our_pages/ib/Banner";
 import React from "react";
+import { useLocale, useTranslations } from "next-intl"
 
-const page = ({ params: locale }) => {
-  console.log(locale, "locale");
+const page = () => {
+  const locale = useLocale()
+  console.log(locale, "hell");
+  const t = useTranslations("ib")
+  
   return (
     <>
-      <Banner />
+    { t("header.title") }
+      {/* <Banner /> */}
     </>
   );
 };
