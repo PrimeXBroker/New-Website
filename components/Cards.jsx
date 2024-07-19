44bc0f1a7d2f1d507f6bb1c4ecf91f68acc89f08
ShanzaTanveer <shanza@primexbroker.com>
2024-07-19 12:06:18 +0500

    ss

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -5,9 +5,12 @@ const Cards = ({ items }) => {
   return (
     <section className="py-12">
       <div className="flex flex-row mt-12 gap-12 justify-center flex-wrap">
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
-            <div className="bg-secondary rounded-lg shadow-2xl border-b-5 border-primary hover:border-primary p-8 w-[300px] transform transition-transform duration-300 hover:scale-105 group">
+            <div
+              className="bg-secondary rounded-lg shadow-2xl border-b-5 border-primary hover:border-primary p-8 w-[300px] transform transition-transform duration-300 hover:scale-105 group"
+              key={index}
+            >
               <div className="flex justify-center">
                 <Image
                   src={item.img}
