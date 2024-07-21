35132efd9e74adea26f864e95ef3697a9605f94b
jarko1985 <hassan.jarko@yahoo.com>
2024-07-21 18:03:38 +0400

    added forex table

---
+++ app/[locale]/(routes)/(instruments)/forex/page.jsx
@@ -0,0 +1,12 @@
+import ForexTable from "@/our_pages/instruments/forex/ForexTable";
+import React from "react";
+
+const Forex = () => {
+  return (
+    <>
+      <ForexTable />
+    </>
+  );
+};
+
+export default Forex;
