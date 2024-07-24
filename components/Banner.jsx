5f7c188c240f9115002bf6c0715d03b9f3024556
jarko1985 <hassan.jarko@yahoo.com>
2024-07-24 22:24:58 +0400

    finished awards page

---
+++ components/Banner.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import React from "react";
+
+const Banner = ({
+  background,
+  title,
+  titleColor,
+  description,
+  descriptionColor,
+  imgUrl,
+  imgWidth,
+  imgHeight,
+  children,
+}) => {
+  return (
+    <section className={`${background}`}>
+      <div className="container flex flex-col md:flex-row justify-evenly gap-10 items-center py-12">
+        <div className="md:w-[40%]">
+          <h1
+            className={`${titleColor} md:text-3xl font-semibold mb-5`}
+            style={{ letterSpacing: "1px" }}
+          >
+            {title}
+          </h1>
+          <p
+            className={`${descriptionColor} text-lg font-[500]`}
+            style={{ letterSpacing: "1.4px" }}
+          >
+            {description}
+          </p>
+          {children}
+        </div>
+        <div>
+          <Image
+            src={imgUrl}
+            width={imgWidth}
+            height={imgHeight}
+            alt="banner Image"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
