9d0d49ae89332de1fc2f7dbb313103ae445af5f8
Prime X <143796418+PrimeXBroker@users.noreply.github.com>
2024-08-01 18:59:32 +0400

    chat_app

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -61,21 +61,21 @@ export default async function layout({ children, params: { locale } }) {
   return (
     <html className={montserrat.variable} lang={locale} dir={direction}>
       <Head>
-      {/* <div id="chat_app"></div> */}
-      <script src="https://primexbroker.online/static/js/main.js"></script>
-      <link rel="stylesheet" href="https://primexbroker.online/static/css/main.css" />
+        <div id="chat_app"></div>
+        <script src="https://primexbroker.online/static/js/main.js"></script>
+        <link rel="stylesheet" href="https://primexbroker.online/static/css/main.css" />
       </Head>
       <body>
         <NextUIProvider>
           <NextIntlClientProvider messages={messages}>
             <Header locale={locale} />
             <Suspense fallback={null}>
-            {children}
-            <Toaster
-            toastOptions={{
-              duration:5000
-            }}
-            />
+              {children}
+              <Toaster
+                toastOptions={{
+                  duration: 5000
+                }}
+              />
             </Suspense>
             <Footer />
             <Cookies />
