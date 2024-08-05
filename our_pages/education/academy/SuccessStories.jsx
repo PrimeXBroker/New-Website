41d94f6b8a81abaa607a508b4286cf80033d7dd2
Prime X <143796418+PrimeXBroker@users.noreply.github.com>
2024-08-05 10:04:35 +0400

    ,

--- our_pages/education/academy/SuccessStories.jsx
+++ our_pages/education/academy/SuccessStories.jsx
@@ -25,6 +25,37 @@ const successData = [
   },
 ];
 
+const reviews = [
+  {
+    name: "Rahimmm",
+    title: "It's one of the best platforms o",
+    description: " It’s one of the best platforms of trading and I like the service customers and Nour Al Ganna she helped me very much thanks PrimeX.",
+    country: "🇩🇿 DZ",
+    date: "April 17, 2024"
+  },
+  {
+    name: "Hassan",
+    title: "It's one of the best platforms o",
+    description: " It’s one of the best platforms of trading and I like the service customers and Nour Al Ganna she helped me very much thanks PrimeX.",
+    country: "🇩🇿 DZ",
+    date: "April 17, 2024"
+  },
+  {
+    name: "AHmed",
+    title: "It's one of the best platforms o",
+    description: " It’s one of the best platforms of trading and I like the service customers and Nour Al Ganna she helped me very much thanks PrimeX.",
+    country: "🇩🇿 DZ",
+    date: "April 17, 2024"
+  },
+  {
+    name: "Ali",
+    title: "It's one of the best platforms o",
+    description: " It’s one of the best platforms of trading and I like the service customers and Nour Al Ganna she helped me very much thanks PrimeX.",
+    country: "🇩🇿 DZ",
+    date: "April 17, 2024"
+  },
+]
+
 const SuccessStories = () => {
   const t = useTranslations("academy.successStories");
 
@@ -36,19 +67,47 @@ const SuccessStories = () => {
       <p className="text-secondary text-[1rem] md:text-lg text-center max-w-xl mx-auto">
         {t("description")}
       </p>
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 place-content-center items-center place-items-center pt-6">
-        {successData.map((el) => (
-          <div key={el.id}>
-            <Image
-              src={el.imgUrl}
-              width="350"
-              height="100"
-              alt="users"
-              className="md:w-[100%] "
-            />
-          </div>
-        ))}
+
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 place-content-center items-center place-items-center mt-5">
+        {
+          reviews.map((review, index) => (
+            <div className="max-w-sm mx-auto bg-[#F3F3F3] rounded-lg shadow-md p-4">
+              <div className="flex items-center mb-4">
+                <div className="w-10 h-10 p-4 rounded-full bg-white flex items-center justify-center text-xl font-bold">
+                  RA
+                </div>
+                <div className="ml-4">
+                  <div className="font-semibold">{review.name}</div>
+                  <div className="text-sm text-gray-500">1 review <span className="ml-2">{review.country}</span></div>
+                </div>
+              </div>
+              <div className="flex items-center mb-4">
+                <div className="flex items-center gap-1">
+                  {[...Array(5)].map((_, index) => (
+                    <div className="bg-[#53B07F] p-1">
+                      <svg
+                        key={index}
+                        className="w-4 h-4 text-[#fff]"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.974h4.174c.97 0 1.371 1.24.588 1.81l-3.368 2.448 1.286 3.974c.3.921-.755 1.688-1.541 1.117l-3.368-2.448-3.368 2.448c-.786.571-1.841-.196-1.541-1.117l1.286-3.974-3.368-2.448c-.783-.57-.382-1.81.588-1.81h4.174l1.286-3.974z" />
+                      </svg>
+                    </div>
+
+                  ))}
+                </div>
+                <div className="text-sm text-gray-500 ml-auto">{review.date}</div>
+              </div>
+              <div className="font-semibold mb-2">{review.title}</div>
+              <div className="text-gray-700 text-sm">
+                {review.description}
+              </div>
+            </div>
+          ))
+        }
       </div>
+
     </section>
   );
 };
