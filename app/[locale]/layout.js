16102545766031129988aaa283cb675bb8d0709b
primexjarko <hassan.jarko@primexbroker.com>
2024-08-07 16:43:05 +0400

    added goggle tag manager

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -82,28 +82,21 @@ export default async function layout({ children, params: { locale } }) {
           </div>
           <ChatWidget />
         </Suspense>
-        <Script
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html:`
-        function gtag_report_conversion(url) {
-          var callback = function () {
-            if (typeof(url) != 'undefined') {
-              window.location = url;
-            }
-          };
-          gtag('event', 'conversion', {
-              'send_to': 'AW-10835048699/LUb0CNmY5OsYEPvxxq4o',
-              'event_callback': callback
-          });
-          return false;
-        }
-           document.addEventListener("wpcf7submit", function(event){
-            gtag_report_conversion();
-          }, false);
-        `
-      }}
-      />
+        <Script id="gtm" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer',''GTM-M3KSBZMM'');
+      `}
+    </Script>
+    <noscript
+        dangerouslySetInnerHTML={{
+        __html: ` <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3KSBZMM"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} 
+    	/>
       </body>
     </html>
   );
