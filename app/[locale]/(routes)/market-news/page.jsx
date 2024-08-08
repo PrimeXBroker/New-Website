c3b629403a71dc5daf408516128aced89cbcb822
Prime X <143796418+PrimeXBroker@users.noreply.github.com>
2024-08-08 12:05:57 +0400

    m

---
+++ app/[locale]/(routes)/market-news/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '@/our_pages/market-news/Banner'
+
+function page() {
+  return (
+    <div>
+        <Banner />
+    </div>
+  )
+}
+
+export default page
