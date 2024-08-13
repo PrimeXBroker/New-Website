7870be7c4b65234478975778023bf3f2b4dae76e
ShanzaTanveer <shanza@primexbroker.com>
2024-08-13 14:12:00 +0500

    cookies section

--- components/CookiesBanner.jsx
+++ components/CookiesBanner.jsx
@@ -32,12 +32,12 @@ const CookieBanner = () => {
         <div className="w-full bg-secondary z-50 fixed bottom-0 text-white p-2">
           <div className="container">
             <div className="grid grid-cols-1 items-center md:grid-cols-12 gap-4">
-              <p className="text-[13px] ltr:text-left rtl:text-right col-span-1 md:col-span-10">
+              <p className="text-[11px] text-center md:ltr:text-left md:rtl:text-right col-span-1 md:col-span-10">
                 {t("text")}
               </p>
               <div className="md:text-right text-center col-span-1 md:col-span-2">
                 <button
-                  className="text-white uppercase bg-primary text-sm px-3 py-[6px] gap-2 items-center hover:bg-white hover:text-primary"
+                  className="text-secondary uppercase bg-primary text-[11px] rounded-full px-3 py-[5px] gap-2 items-center hover:bg-white hover:text-secondary"
                   onClick={() => {
                     setCookie("primex_cookie", "true", { maxAge: 2592000 }); // 30 days
                     setShow(false);
