f056bc0b89e7221a468af6b017cf27398e824d84
ShanzaTanveer <shanza@primexbroker.com>
2024-09-13 18:59:38 +0500

    ss

--- app/[locale]/(routes)/funded-account-competition/page.jsx
+++ app/[locale]/(routes)/funded-account-competition/page.jsx
@@ -1,50 +1,43 @@
-"use client";
-import Banner from "@/our_pages/demo-account/Banner";
-import FundedAccount from "@/our_pages/demo-account/FundedAccount";
-import SignUp from "@/our_pages/demo-account/SignUp";
+import DemoAccountWrapper from "@/our_pages/demo-account/DemoAccountWrapper";
 import React from "react";
-import Faq from "@/components/instrments/common/Faq";
-import { useTranslations } from "next-intl";
-import ParticipateForm from "@/our_pages/demo-account/ParticipateForm";
+import { createTranslator } from "next-intl";
+
+export async function generateMetadata({ params: { locale } }) {
+  const messages = (await import(`../../../../messages/${locale}.json`))
+    .default;
+  const t = createTranslator({ locale, messages });
+  const url =
+    locale != "en"
+      ? `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}/funded-account-competition`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/funded-account-competition`;
+  return {
+    title: t("demoAccount.metaData.title"),
+    description: t("demoAccount.metaData.description"),
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      type: "website",
+      locale: locale,
+      url: url,
+      title: t("demoAccount.metaData.title"),
+      description: t("demoAccount.metaData.description"),
+      images: [
+        {
+          url: "https://primexcapital.s3.eu-north-1.amazonaws.com/website/demo-account/demo-account-banner.webp",
+          width: 1200,
+          height: 630,
+          alt: t("demoAccount.metaData.title"),
+        },
+      ],
+    },
+  };
+}
 
 const page = () => {
-  const t = useTranslations("demoAccount");
-
-  const forexFaqs = [
-    {
-      key: 1,
-      question: t("faq.question_1"),
-      answer: t("faq.answer_1"),
-    },
-    {
-      key: 2,
-      question: t("faq.question_2"),
-      answer: t("faq.answer_2"),
-    },
-    {
-      key: 3,
-      question: t("faq.question_3"),
-      answer: t("faq.answer_3"),
-    },
-    {
-      key: 4,
-      question: t("faq.question_4"),
-      answer: t("faq.answer_4"),
-    },
-    {
-      key: 5,
-      question: t("faq.question_5"),
-      answer: t("faq.answer_5"),
-    },
-  ];
-
   return (
     <>
-      <Banner />
-      <FundedAccount />
-      <ParticipateForm />
-      <SignUp />
-      <Faq forexFaqs={forexFaqs} />
+      <DemoAccountWrapper />
     </>
   );
 };
---
+++ our_pages/demo-account/DemoAccountWrapper.jsx
@@ -0,0 +1,52 @@
+"use client";
+import Banner from "@/our_pages/demo-account/Banner";
+import FundedAccount from "@/our_pages/demo-account/FundedAccount";
+import SignUp from "@/our_pages/demo-account/SignUp";
+import React from "react";
+import Faq from "@/components/instrments/common/Faq";
+import { useTranslations } from "next-intl";
+import ParticipateForm from "@/our_pages/demo-account/ParticipateForm";
+
+const DemoAccountWrapper = () => {
+  const t = useTranslations("demoAccount");
+
+  const forexFaqs = [
+    {
+      key: 1,
+      question: t("faq.question_1"),
+      answer: t("faq.answer_1"),
+    },
+    {
+      key: 2,
+      question: t("faq.question_2"),
+      answer: t("faq.answer_2"),
+    },
+    {
+      key: 3,
+      question: t("faq.question_3"),
+      answer: t("faq.answer_3"),
+    },
+    {
+      key: 4,
+      question: t("faq.question_4"),
+      answer: t("faq.answer_4"),
+    },
+    {
+      key: 5,
+      question: t("faq.question_5"),
+      answer: t("faq.answer_5"),
+    },
+  ];
+
+  return (
+    <>
+      <Banner />
+      <FundedAccount />
+      <ParticipateForm />
+      <SignUp />
+      <Faq forexFaqs={forexFaqs} />
+    </>
+  );
+};
+
+export default DemoAccountWrapper;
