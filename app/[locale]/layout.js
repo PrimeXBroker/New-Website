09b6bea71de6f52188e24f2f55df76218e4f2b23
ShanzaTanveer <shanza@primexbroker.com>
2024-09-17 16:16:34 +0500

    ss

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -252,6 +252,13 @@ export default async function layout({ children, params: { locale } }) {
             `,
           }}
         />
+        <Script
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "o4jb4ltpwi");`,
+          }}
+        />
       </body>
     </html>
   );
