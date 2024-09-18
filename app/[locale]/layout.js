f954e84a8ed01c93b92758051d8e4747278e2b70
Prime X <143796418+PrimeXBroker@users.noreply.github.com>
2024-09-18 12:08:18 +0400

    m

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -150,6 +150,14 @@ export default async function layout({ children, params: { locale } }) {
           href="https://www.primexcapital.com/en"
           hreflang="x-default"
         />
+         <Script
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "o4jb4ltpwi");`,
+          }}
+        />
+        
       </Head>
       <GoogleAnalytics />
       <body>
@@ -252,13 +260,7 @@ export default async function layout({ children, params: { locale } }) {
             `,
           }}
         />
-        <Script
-          id="clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "o4jb4ltpwi");`,
-          }}
-        />
+       
       </body>
     </html>
   );
