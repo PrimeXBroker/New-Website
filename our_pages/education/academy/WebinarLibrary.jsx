c00e93c25ad0693dd564816f4c594c2d791cb6e3
ShanzaTanveer <shanza@primexbroker.com>
2024-09-25 11:59:31 +0500

    ss

--- our_pages/education/academy/WebinarLibrary.jsx
+++ our_pages/education/academy/WebinarLibrary.jsx
@@ -117,6 +117,7 @@ const WebinarLibrary = () => {
           autoplay={{
             delay: 1500,
             disableOnInteraction: false,
+            pauseOnMouseEnter: true,
           }}
           spaceBetween={20}
           loop
