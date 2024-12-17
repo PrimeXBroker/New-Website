c2a47186a72fdf22faa70f246886b0c02224af72
ShanzaTanveer <shanza@primexbroker.com>
2024-12-17 12:29:53 +0500

    ss

--- app/robots.js
+++ app/robots.js
@@ -7,7 +7,21 @@ export default function robots() {
     },
     sitemap: [
       "https://www.primexcapital.com/sitemap.xml",
-      "https://www.primexcapital.com/sitemap/market-news-en.xml",
+      "https://primexcapital.com/sitemap/en.xml",
+      "https://primexcapital.com/sitemap/ar.xml",
+      "https://primexcapital.com/sitemap/fa.xml",
+      "https://primexcapital.com/sitemap/kur.xml",
+      "https://primexcapital.com/sitemap/tr.xml",
+      "https://primexcapital.com/sitemap/market-news-en.xml",
+      "https://primexcapital.com/sitemap/market-news-ar.xml",
+      "https://primexcapital.com/sitemap/starting-gateway-en.xml",
+      "https://primexcapital.com/sitemap/starting-gateway-ar.xml",
+      "https://primexcapital.com/sitemap/press-release-en.xml",
+      "https://primexcapital.com/sitemap/press-release-ar.xml",
+      "https://primexcapital.com/sitemap/technical-analysis-en.xml",
+      "https://primexcapital.com/sitemap/technical-analysis-ar.xml",
+      "https://primexcapital.com/sitemap/learning-hub-en.xml",
+      "https://primexcapital.com/sitemap/learning-hub-ar.xml",
     ],
   };
 }
