68b56f2e9a5b9cbe308b6f2400f5868cdb37217b
ShanzaTanveer <shanza@primexbroker.com>
2024-12-17 16:44:55 +0500

    ss

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,9 +1,16 @@
 import HomePage from "@/our_pages/home/HomePage";
 import Script from "next/script";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="dkhacq1ppi9o8sjisuboei4diydc3g"
+        />
+      </Head>
       <Script
         type="application/ld+json"
         id="organization-schema"
