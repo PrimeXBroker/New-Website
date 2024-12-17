f53f8b67b04b53486e23b58f8b7ef0ea271cecda
ShanzaTanveer <shanza@primexbroker.com>
2024-12-17 12:22:18 +0500

    ss

--- app/sitemap/market-news-en.xml/route.js
+++ app/sitemap/market-news-en.xml/route.js
@@ -1,21 +1,28 @@
+function escapeXML(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/'/g, "&apos;")
+    .replace(/"/g, "&quot;");
+}
+
 async function fetchEnglishMarketNews() {
   const categoryId = "664dd9c93f02939fcd48959e";
   try {
     const response = await fetch(
-      `https://primexbroker.com/api/fetch/market-news-en-slugs/${categoryId}`
+      `https://primexcapital.com/api/fetch/market-news-en-slugs/${categoryId}`
     );
     const data = await response.json();
     const links = data.map((item) => ({
-      url: `https://primexcapital.com/en/market-news-detail/${item.slug}`,
-      lastModified: new Date(item.lastModified || Date.now()),
+      url: `https://primexcapital.com/en/market-news-detail/${escapeXML(
+        item.slug
+      )}`,
+      title: escapeXML(item.title),
+      language: escapeXML(item.language),
+      createdOn: new Date(item.createdOn).toISOString(),
     }));
-    return links.map((link) => ({
-      ...link,
-      url: link.url
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;"),
-    }));
+    return links;
   } catch (error) {
     console.error("Failed to fetch dynamic links:", error);
     return [];
@@ -26,17 +33,28 @@ export async function GET() {
   const marketNewsEnUrls = await fetchEnglishMarketNews();
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${marketNewsEnUrls
-        .map(
-          ({ url, lastModified }) => `
-        <url>
-          <loc>${url}</loc>
-          <lastmod>${lastModified.toISOString()}</lastmod>
-        </url>`
-        )
-        .join("")}
-    </urlset>`;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+    ${marketNewsEnUrls
+      .map(
+        (article) => `
+      <url>
+        <loc>${article.url}</loc>
+        <news:news>
+          <news:publication>
+            <news:name>PrimeX Capital Market News</news:name>
+            <news:language>${article.language}</news:language>
+          </news:publication>
+          <news:publication_date>${new Date(
+            article.createdOn
+          ).toISOString()}</news:publication_date>
+          <news:title>${article.title}</news:title>
+        </news:news>
+      </url>`
+      )
+      .join("")}
+  </urlset>`;
+
   return new Response(xml, {
     headers: {
       "Content-Type": "application/xml; charset=utf-8",
--- app/sitemap/press-release-ar.xml/route.js
+++ app/sitemap/press-release-ar.xml/route.js
@@ -1,3 +1,12 @@
+function escapeXML(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/'/g, "&apos;")
+    .replace(/"/g, "&quot;");
+}
+
 async function fetchArabicPressRelease() {
   const categoryId = "665448dcf5b226a0bd9b574e";
   try {
@@ -6,8 +15,12 @@ async function fetchArabicPressRelease() {
     );
     const data = await response.json();
     const links = data.map((item) => ({
-      url: `https://primexcapital.com/ar/market-news-detail/${item.slug}`,
-      lastModified: new Date(item.lastModified || Date.now()),
+      url: `https://primexcapital.com/ar/market-news-detail/${escapeXML(
+        item.slug
+      )}`,
+      title: escapeXML(item.title),
+      language: escapeXML(item.language),
+      createdOn: new Date(item.createdOn).toISOString(),
     }));
     return links.map((link) => ({
       ...link,
@@ -24,18 +37,30 @@ async function fetchArabicPressRelease() {
 
 export async function GET() {
   const pressReleaseArUrls = await fetchArabicPressRelease();
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
     ${pressReleaseArUrls
       .map(
-        ({ url, lastModified }) => `
+        (article) => `
       <url>
-        <loc>${url}</loc>
-        <lastmod>${lastModified.toISOString()}</lastmod>
+        <loc>${article.url}</loc>
+        <news:news>
+          <news:publication>
+            <news:name>PrimeX Capital Market News</news:name>
+            <news:language>${article.language}</news:language>
+          </news:publication>
+          <news:publication_date>${new Date(
+            article.createdOn
+          ).toISOString()}</news:publication_date>
+          <news:title>${article.title}</news:title>
+        </news:news>
       </url>`
       )
       .join("")}
   </urlset>`;
+
   return new Response(xml, {
     headers: {
       "Content-Type": "application/xml; charset=utf-8",
