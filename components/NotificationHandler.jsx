f37c3f728edb4f8a1fdda49e5d9449be1960778a
ShanzaTanveer <shanza@primexbroker.com>
2024-12-26 14:50:21 +0500

    ss

--- components/NotificationHandler.jsx
+++ components/NotificationHandler.jsx
@@ -4,30 +4,38 @@ import { useEffect } from "react";
 
 const NotificationHandler = () => {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log("Service Worker registered:", registration.scope);
-        })
-        .catch((err) => {
-          console.error("Service Worker registration failed:", err);
-        });
-    }
+    const isIOS =
+      /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    if (!isIOS) {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((registration) => {
+            console.log("Service Worker registered:", registration.scope);
+          })
+          .catch((err) => {
+            console.error("Service Worker registration failed:", err);
+          });
+      }
 
-    if (Notification.permission === "default") {
-      Notification.requestPermission()
-        .then((permission) => {
-          if (permission === "granted") {
-            console.log("Notification permission granted.");
-            SubscribeNotificationUser();
-          } else {
-            console.warn("Notification permission denied.");
-          }
-        })
-        .catch((err) => {
-          console.error("Notification permission error:", err);
-        });
+      if (Notification.permission === "default") {
+        Notification.requestPermission()
+          .then((permission) => {
+            if (permission === "granted") {
+              console.log("Notification permission granted.");
+              SubscribeNotificationUser();
+            } else {
+              console.warn("Notification permission denied.");
+            }
+          })
+          .catch((err) => {
+            console.error("Notification permission error:", err);
+          });
+      }
+    } else {
+      console.log(
+        "iOS device detected - Skipping service worker and notifications registration."
+      );
     }
   }, []);
 
