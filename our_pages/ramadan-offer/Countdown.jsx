4555d6639cfd71fbedcb283c2323e9bcd741e0fe
ShanzaTanveer <shanza@primexbroker.com>
2025-02-26 18:09:40 +0500

    s

--- our_pages/ramadan-offer/Countdown.jsx
+++ our_pages/ramadan-offer/Countdown.jsx
@@ -33,44 +33,44 @@ const Countdown = () => {
   const { days, hours, minutes, secs } = formatTime(timeLeft);
 
   return (
-    <div className="bg-[#000000] pt-14">
+    <div className="bg-[#000000] pt-4 md:pt-14">
       <div className="container">
         <div className="flex flex-col lg:flex-row items-center justify-between">
           <div>
             <h2 className="text-3xl sm:text-4xl lg:text-5xl text-[#ffffff] font-semibold">
               {t("title1")}
             </h2>
-            <h2 className="text-3xl sm:text-4xl lg:text-5xl text-[#FED100] font-semibold mt-2">
+            <h2 className="text-3xl sm:text-4xl lg:text-5xl text-[#FED100] font-semibold mt-1 lg:mt-2">
               {t("title2")}
             </h2>
           </div>
-          <div className="flex gap-4 text-3xl sm:text-4xl lg:text-5xl font-bold text-[#ffffff]">
+          <div className="flex gap-2 sm:gap-4 text-3xl sm:text-4xl lg:text-5xl font-bold text-[#ffffff] mt-8 lg:mt-0">
             <div className="text-center">
-              <span className="text-6xl font-semibold">
+              <span className="text-5xl sm:text-6xl font-semibold">
                 {String(days).padStart(2, "0")}
               </span>
-              <p className="text-sm font-medium">{t("days")}</p>
+              <p className="text-xs sm:text-sm font-medium">{t("days")}</p>
             </div>
             <span>:</span>
             <div className="text-center">
-              <span className="text-6xl font-semibold">
+              <span className="text-5xl sm:text-6xl font-semibold">
                 {String(hours).padStart(2, "0")}
               </span>
-              <p className="text-sm font-medium">{t("hours")}</p>
+              <p className="text-xs sm:text-sm font-medium">{t("hours")}</p>
             </div>
             <span>:</span>
             <div className="text-center">
-              <span className="text-6xl font-semibold">
+              <span className="text-5xl sm:text-6xl font-semibold">
                 {String(minutes).padStart(2, "0")}
               </span>
-              <p className="text-sm font-medium">{t("minutes")}</p>
+              <p className="text-xs sm:text-sm font-medium">{t("minutes")}</p>
             </div>
             <span>:</span>
             <div className="text-center">
-              <span className="text-6xl font-semibold">
+              <span className="text-5xl sm:text-6xl font-semibold">
                 {String(secs).padStart(2, "0")}
               </span>
-              <p className="text-sm font-medium">{t("seconds")}</p>
+              <p className="text-xs sm:text-sm font-medium">{t("seconds")}</p>
             </div>
           </div>
         </div>
--- our_pages/ramadan-offer/ExclusivePrizes.jsx
+++ our_pages/ramadan-offer/ExclusivePrizes.jsx
@@ -20,23 +20,24 @@ const ExclusivePrizes = () => {
         "https://primexcapital.s3.eu-north-1.amazonaws.com/website/ramadan-offer/iphone-16-pro.webp",
     },
     {
-      title: t("name3"),
-      name: "Samsung Galaxy S25",
+      title: t("title3"),
+      name: t("name3"),
       image:
         "https://primexcapital.s3.eu-north-1.amazonaws.com/website/ramadan-offer/samsung-galaxy-s25.webp",
     },
   ];
 
   return (
-    <div className="bg-[#000000] py-24">
+    <div className="bg-[#000000] py-14 md:py-24">
       <div className="container">
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl font-semibold text-[#ffffff] text-center">
+        <h2 className="text-3xl sm:text-3xl lg:text-4xl font-semibold text-[#ffffff] text-center">
           <span className="text-[#FED100]">{t("main_title1")}</span>
+          <br className="sm:hidden" />
           {t("main_title2")}
         </h2>
-        <div className="mt-10 flex justify-center gap-6">
+        <div className="mt-8 sm:mt-10 flex xs:flex-col sm:flex-wrap md:flex-nowrap md:flex-row justify-center gap-6">
           {prizes.map((prize, index) => (
-            <div key={index} className="w-1/3 text-center">
+            <div key={index} className="w-full sm:w-[48%] md:w-1/3 text-center">
               <div className="relative">
                 <Image
                   unoptimized={true}
@@ -51,7 +52,7 @@ const ExclusivePrizes = () => {
                     {prize.title}
                   </div>
                 </div>
-                <h3 className="absolute top-16 px-5 mt-2 text-2xl font-medium text-[#ffffff]">
+                <h3 className="absolute top-16 px-5 mt-2 text-2xl md:text-lg lg:text-2xl font-medium text-[#ffffff]">
                   {prize.name}
                 </h3>
               </div>
