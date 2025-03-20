3a930cda983bfe1d6c90d3c1d35f7dcfa3216e54
Mehtabkazmi <mehtabkazmi5@gmail.com>
2025-03-20 14:59:37 +0500

    meta data in local despositor

--- app/[locale]/(routes)/(social-trading)/local-depositor/page.jsx
+++ app/[locale]/(routes)/(social-trading)/local-depositor/page.jsx
@@ -1,11 +1,46 @@
 import LocalDepositorWrapper from "@/our_pages/local-depositor/LocalDepositorWrapper";
+import { createTranslator } from "next-intl";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const LocalDepositor = ({ params: { locale } }) => {
+export async function generateMetadata({ params: { locale } }) {
+  const messages = (await import(`../../../../../messages/${locale}.json`))
+    .default;
+  const t = createTranslator({ locale, messages });
+  const url =
+    locale != "en"
+      ? `${process.env.NEXT_PUBLIC_BASE_URL}/${locale}/local-depositor`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/local-depositor`;
+
+  return {
+    title: t("localDepositor.metaData.title"),
+    description: t("localDepositor.metaData.description"),
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      type: "website",
+      locale,
+      url,
+      title: t("localDepositor.metaData.title"),
+      description: t("localDepositor.metaData.description"),
+      images: [
+        {
+          url: "https://primexcapital.s3.eu-north-1.amazonaws.com/website/local-depositor/hero.webp",
+          width: 1200,
+          height: 630,
+          alt: t("localDepositor.metaData.title"),
+        },
+      ],
+    },
+  };
+}
+
+const LocalDepositor = async ({ params: { locale } }) => {
   if (locale !== "ar") {
     redirect("/");
   }
+
   return <LocalDepositorWrapper />;
 };
 
