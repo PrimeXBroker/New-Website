abc1f4179a9dfa9bd84740de252fc6d620ebac2a
ShanzaTanveer <shanza@primexbroker.com>
2025-04-30 13:41:08 +0500

    s

--- app/[locale]/(routes)/primex-capital-app/page.jsx
+++
@@ -1,8 +0,0 @@
-import PrimeXAppWrapper from "@/our_pages/primex-capital-app/PrimexAppWrapper";
-import React from "react";
-
-const page = () => {
-  return <PrimeXAppWrapper />;
-};
-
-export default page;
