387d9e48edd33389d57e163f329580825920d72f
ShanzaTanveer <shanza@primexbroker.com>
2025-05-08 11:29:45 +0500

    s

--- our_pages/partnerships/ib-program/IbForm.jsx
+++ our_pages/partnerships/ib-program/IbForm.jsx
@@ -8,7 +8,7 @@ const IbForm = () => {
     <section className="bg-p dark:bg-p-dark py-10">
       <div className="container grid grid-cols-12 items-center">
         <div className="col-span-12 lg:col-span-6 mb-7 lg:mb-0 text-center lg:text-start">
-          <div className="hidden lg:block">
+          <div>
             <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-tm dark:text-tm-dark mb-2">
               {t("main_title1")}
             </h2>
@@ -30,7 +30,7 @@ const IbForm = () => {
               </span>
             </h2>
           </div>
-          <div className="block lg:hidden text-center">
+          {/* <div className="block lg:hidden text-center">
             <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-tm dark:text-tm-dark mb-2">
               {t("main_title1")}
               <span className="text-pcp dark:text-pcp-dark">
@@ -47,7 +47,7 @@ const IbForm = () => {
                 {t("main_title6")}
               </span>
             </h2>
-          </div>
+          </div> */}
         </div>
         <div className="col-span-12 lg:col-span-6 ">
           <Form />
