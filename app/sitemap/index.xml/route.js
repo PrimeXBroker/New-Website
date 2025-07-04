b0c658be8c7fbfff7e65df9e7c1c6d6d3a97de5e
ShanzaTanveer <shanza@primexbroker.com>
2025-07-04 13:55:22 +0500

    s

--- app/sitemap/index.xml/route.js
+++ app/sitemap/index.xml/route.js
@@ -10,62 +10,62 @@ export async function GET() {
     {
       url: `${baseUrl}/sitemap/market-news-en.xml`,
       changefreq: "hourly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/market-news-ar.xml`,
       changefreq: "hourly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/technical-analysis-en.xml`,
       changefreq: "weekly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/technical-analysis-ar.xml`,
       changefreq: "weekly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/blogs-en.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/blogs-ar.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/starting-gateway-en.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/starting-gateway-ar.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/press-release-en.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/press-release-ar.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/learning-hub-en.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
     {
       url: `${baseUrl}/sitemap/learning-hub-ar.xml`,
       changefreq: "monthly",
-      priority: "0.7",
+      priority: "0.8",
     },
   ];
 
