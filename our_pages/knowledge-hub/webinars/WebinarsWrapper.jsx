9dd2440d881f7e0ad111bc06779d56118840a60a
ShanzaTanveer <shanza@primexbroker.com>
2025-07-23 12:16:57 +0500

    s

--- our_pages/knowledge-hub/webinars/WebinarsWrapper.jsx
+++ our_pages/knowledge-hub/webinars/WebinarsWrapper.jsx
@@ -1,15 +1,28 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import { useLocale } from "next-intl";
 import Webinars from "@/our_pages/education/academy/Webinars";
 import PrivateSessions from "@/our_pages/education/academy/PrivateSessions";
 import Banner from "./Banner";
+import { useSearchParams } from "next/navigation";
 
 const WebinarsWrapper = () => {
   const t = useTranslations("academy.joinAcademy");
+  const searchParams = useSearchParams();
+  const tabParam = searchParams.get("tab");
   const [active, setActive] = useState("Webinars");
   const locale = useLocale();
+
+  useEffect(() => {
+    if (tabParam === "session") {
+      setActive("Session");
+      const section = document.getElementById("academy-form");
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [tabParam]);
 
   return (
     <>
