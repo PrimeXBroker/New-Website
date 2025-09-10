b67e353d99cfb811cd7bb8c26f4cc329022c5507
CodeScrapper1 <codescrapper1@gmail.com>
2025-09-10 22:47:56 +0500

    adding intervals

--- app/(trading)/graph/[symbol]/page.jsx
+++ app/(trading)/graph/[symbol]/page.jsx
@@ -3,12 +3,13 @@ import { convertToSeconds } from "@/utils/data";
 import axios from "axios";
 import React from "react";
 
-const Graph = async ({ params }) => {
+const Graph = async ({ params,searchParams }) => {
   const { symbol } = params;
+  const {interval,mode} = searchParams
   console.log(symbol, "params");
 
 
-  return <GraphComp symbol={symbol}/>;
+  return <GraphComp symbol={symbol} interval={parseInt(interval || 1)} mode={mode}/>;
 };
 
 export default Graph;
