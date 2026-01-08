3e6a5ecf177c203bdffd58b05c8355afcb8d65ec
ShanzaTanveer <shanza@primexbroker.com>
2026-01-08 17:40:31 +0500

    s

--- our_pages/account-types/AccountTypesPricingTableMobile.jsx
+++ our_pages/account-types/AccountTypesPricingTableMobile.jsx
@@ -214,6 +214,20 @@ const AccountTypesPricingTableMobile = () => {
               {tabContent[activeTab].metaTrader5}
             </div>
           </div>
+          <div className="text-ts dark:text-ts-dark text-sm sm:text-base font-semibold bg-cc dark:bg-cc-dark px-5 py-6">
+            {t("automaticTrading")}
+          </div>
+          <div className="text-center text-ts dark:text-ts-dark text-sm sm:text-base bg-cc dark:bg-cc-dark px-5 py-6">
+            <div
+              className={`${
+                `${tabContent[activeTab].automaticTrading}` === "X"
+                  ? "text-rc dark:text-rc-dark"
+                  : "text-gc dark:text-gc-dark"
+              } bg-e1 dark:bg-e1-dark h-[28px] w-[28px] mx-auto flex justify-center items-center rounded-lg`}
+            >
+              {tabContent[activeTab].automaticTrading}
+            </div>
+          </div>
 
           <div className="text-ts dark:text-ts-dark text-sm sm:text-base font-semibold bg-cc dark:bg-cc-dark px-5 py-6">
             {t("demo")}
