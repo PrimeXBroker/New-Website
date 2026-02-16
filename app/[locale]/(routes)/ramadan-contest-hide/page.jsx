cc6d26ad27e53bd85baea12ecfa63ada5b323b32
ShanzaTanveer <shanza@primexbroker.com>
2026-02-16 16:06:20 +0500

    ramadan page

---
+++ app/[locale]/(routes)/ramadan-contest-hide/page.jsx
@@ -0,0 +1,62 @@
+import RamadanOfferWrappper from "@/our_pages/ramadan-offer/RamadanOfferWrappper";
+import { createTranslator } from "next-intl";
+
+export async function generateMetadata({ params: { locale } }) {
+  const messages = (await import(`../../../../messages/${locale}.json`))
+    .default;
+  const t = createTranslator({ locale, messages });
+  const url = `https://www.primexcapital.com/${locale}/ramadan-contest-hide`;
+
+  return {
+    title: t("ramadanOffer.metaData.title"),
+    description: t("ramadanOffer.metaData.description"),
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      type: "website",
+      locale: locale,
+      url: url,
+      title: t("ramadanOffer.metaData.title"),
+      description: t("ramadanOffer.metaData.description"),
+      images: [
+        {
+          url: "https://primexcapital.s3.eu-north-1.amazonaws.com/website/ramadan-offer/ramadan-contest-hero.webp",
+          width: 1200,
+          height: 630,
+          alt: t("ramadanOffer.metaData.title"),
+        },
+      ],
+    },
+  };
+}
+
+const page = () => {
+  const hreflangLocales = [
+    { lng: "en", url: "en" },
+    { lng: "ar", url: "ar" },
+    { lng: "ku", url: "ku" },
+    { lng: "es", url: "es" },
+    { lng: "ps", url: "ps" },
+    { lng: "pt", url: "pt" },
+    { lng: "fa", url: "fa" },
+  ];
+
+  return (
+    <>
+      <head>
+        {hreflangLocales.map((item) => (
+          <link
+            key={item.lng}
+            rel="alternate"
+            href={`https://www.primexcapital.com/${item.url}/ramadan-contest-hide`}
+            hreflang={item.lng}
+          />
+        ))}
+      </head>
+      <RamadanOfferWrappper />
+    </>
+  );
+};
+
+export default page;
