8424fc5ead19fca0ce92036c35925305b4b82a7a
ShanzaTanveer <shanzatanveer676@gmail.com>
2026-09-23 15:01:11 +0500

    scripts remove

--- app/layout.js
+++ app/layout.js
@@ -82,63 +82,10 @@ export default async function RootLayout({ children }) {
     >
       <head>
         <Script
-          id="clarity"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "o4jb4ltpwi");`,
-          }}
-        />
-        <Script
           id="openai-chatgpt-pixel"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `!function(w,d,s,u){if(w.oaiq)return;var q=function(){q.q.push(arguments)};q.q=[];w.oaiq=q;var j=d.createElement(s);j.async=1;j.src=u;var f=d.getElementsByTagName(s)[0];f.parentNode.insertBefore(j,f)}(window,document,"script","https://bzrcdn.openai.com/sdk/oaiq.min.js");oaiq("init",{pixelId:"Mm2Pz4FWZM8xHV5eFpADh8",debug:true});`,
-          }}
-        />
-        <Script
-          strategy="lazyOnload"
-          src="https://www.googletagmanager.com/gtag/js?id=G-F4WWRCT0TN"
-        />
-        <Script
-          id="google-analytics"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){ dataLayer.push(arguments); }
-              gtag('js', new Date());
-              gtag('config', 'G-F4WWRCT0TN', {
-                'linker': {
-                  'domains': ['primexgrp.com', 'primexcapital.com']
-                }
-              });
-              function gtag_report_conversion(url) {
-                var callback = function () {
-                  if (typeof(url) != 'undefined') {
-                    window.location = url;
-                  }
-                };
-                gtag('event', 'conversion', {
-                    'send_to': 'AW-11492934355/UxvrCKrc_LMaENOFoegq',
-                    'value': 1.0,
-                    'currency': 'AED',
-                    'event_callback': callback
-                });
-                return false;
-              }
-              function gtagSendEvent(url) {
-                var callback = function () {
-                  if (typeof url === 'string') {
-                    window.location = url;
-                  }
-                };
-                gtag('event', 'GA4', {
-                  'event_callback': callback,
-                  'event_timeout': 2000,
-                });
-                return false;
-              }
-            `,
           }}
         />
       </head>
@@ -158,22 +105,6 @@ export default async function RootLayout({ children }) {
             </Providers>
           </Suspense>
         </ReduxProvider>
-        <Script
-          id="tiktok-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
-              var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
-              ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-
-              ttq.load('CRDDOLRC77U00RLLNCIG');
-              ttq.page();
-            }(window, document, 'ttq');
-          `,
-          }}
-        />
         <Script id="gtm" strategy="afterInteractive">
           {`
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -189,91 +120,6 @@ export default async function RootLayout({ children }) {
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
-        <Script
-          id="linkedin-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              _linkedin_partner_id = "7629401";
-              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-              (function(l) {
-                if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-                window.lintrk.q=[]}
-                var s = document.getElementsByTagName("script")[0];
-                var b = document.createElement("script");
-                b.type = "text/javascript";b.async = true;
-                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                s.parentNode.insertBefore(b, s);
-              })(window.lintrk);
-            `,
-          }}
-        />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=7629401&fmt=gif" />`,
-          }}
-        />
-        <Script
-          id="tiktok-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function (w, d, t) {
-                w.TiktokAnalyticsObject=t;
-                var ttq=w[t]=w[t]||[];
-                ttq.methods=[
-                  "page","track","identify","instances","debug","on","off","once","ready","alias","group",
-                  "enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"
-                ];
-                ttq.setAndDefer=function(t,e){
-                  t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}
-                };
-                for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-                ttq.instance=function(t){
-                  for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);
-                  return e
-                };
-                ttq.load=function(e,n){
-                  var r="https://analytics.tiktok.com/i18n/pixel/events.js",
-                  o=n&&n.partner;
-                  ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,
-                  ttq._o=ttq._o||{},ttq._o[e]=n||{};
-                  n=document.createElement("script");
-                  n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;
-                  e=document.getElementsByTagName("script")[0];
-                  e.parentNode.insertBefore(n,e)
-                };
-                ttq.load('CRDDOLRC77U00RLLNCIG');
-                ttq.page();
-              }(window, document, 'ttq');`,
-          }}
-        />
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1089594275856519');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1089594275856519&ev=PageView&noscript=1"
-          />
-        </noscript>
         <Script
           id="livechat"
           strategy="afterInteractive"
